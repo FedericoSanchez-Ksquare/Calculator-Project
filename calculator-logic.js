a7eec64c1a323367298a5c6173e820909892bc41
FedericoSanchez-Ksquare <102171026+FedericoSanchez-Ksquare@users.noreply.github.com>
2022-04-08 11:00:32 -0500

    Adding onclick call to =
    html change, js is detecting a change but shouldnt, js logic for equal is close but not ready, i should update in around an hour

--- calculator-logic.js
+++ calculator-logic.js
@@ -5,7 +5,7 @@ function buttonFun(input) {
   let screen = document.getElementById("screen").value;
   screen = screen + input;
   if (screen == "" && input == 0) {
-   } 
+  }
   document.getElementById("screen").value = screen;
   //screen += screen.setAttribute("value", input);
 }
